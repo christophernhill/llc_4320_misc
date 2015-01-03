8bfc7dfc2a0ecb1eae0e7e71128eb22f2c776bb7
christophernhill <cnh@mit.edu>
2015-01-03 15:27:25 -0500

    Update read_llc4320_hawaii_k.c
    Change to include extracting cutout region with respect to larger region
    (e.g. HOTS region of original Hawaii+ “zone”).
    
    Change to include extracting levels 1 through kcount (for 3d files). 
    Default is to extract just top level if there is no level count command argument.

--- src/read_llc4320_hawaii_k.c
+++ src/read_llc4320_hawaii_k.c
@@ -1,23 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define L_ERRMSG_MAX 1024
+
 main(int argc, char *argv[]) {
  char *infname;
  char errmsg[L_ERRMSG_MAX];
  
  /*
- *       panel_size :: Size of "elemental panel" for this LLC grid
- *                  :: 1/48 - 4320
- *                  :: 1/24 - 2160
- *                  :: 1/12 - 1080
- *       f32_nbytes :: Size of float32
- *       f64_nbytes :: Size of float64
- *       yp0        :: west most cell index
- *       yp1        :: east most cell index
- *       xp0        :: north most cell index
- *       xp1        :: south most cell index
- *  off_base        :: offset to start of panel
- *  off_pan_start   :: offset from start of panel to start of patch to extract
- *      nrec        :: Number of records to read
- *      nel_per_rec :: Number of elements per record
- *                                                                             
+   panel_size :: Size of "elemental panel" for this LLC grid
+              :: 1/48 - 4320
+              :: 1/24 - 2160
+              :: 1/12 - 1080
+   f32_nbytes :: Size of float32
+   f64_nbytes :: Size of float64
+   yp0        :: west most cell index
+   yp1        :: east most cell index
+   xp0        :: north most cell index
+   xp1        :: south most cell index
+   off_base        :: offset to start of panel
+   off_pan_start   :: offset from start of panel to start of patch to extract
+   nrec        :: Number of records to read
+   nel_per_rec :: Number of elements per record
  */
  int panel_size = 4320;
  int f32_nbytes=4;
@@ -26,6 +30,11 @@ main(int argc, char *argv[]) {
  int yp1=3500;
  int xp0=3000;
  int xp1=4320;
+ /* Select HOTS centered 150x150 cutout */
+ yp0=yp0+1300;
+ yp1=yp0+150;
+ xp0 = xp0+750;
+ xp1 = xp0+150;
 
  int off_base=panel_size*panel_size*7;
  int off_pan_start=panel_size*3*(yp0-1)+xp0;
@@ -37,6 +46,7 @@ main(int argc, char *argv[]) {
  int j;
 
  int koff=0;
+ int kcount=1;
  int nread;
  
  int nbyte_cur=f32_nbytes;
@@ -51,12 +61,11 @@ main(int argc, char *argv[]) {
  }
 
  if ( argc == 4 ) {
-  nread=sscanf(argv[3],"%d",&koff);
+  nread=sscanf(argv[3],"%d",&kcount);
   if ( nread != 1 ) {
-   printf("Usage: %s FILE_NAME_TO_READ FILE_NAME_TO_WRITE [klev]\n",argv[0]);
+   printf("Usage: %s FILE_NAME_TO_READ FILE_NAME_TO_WRITE [kcount]\n",argv[0]);
    exit(0);
   }
-  koff=koff-1;
  }
 
  fin_hndl = fopen(argv[1],"r");
@@ -75,25 +84,28 @@ main(int argc, char *argv[]) {
 
  reg_buf=malloc(nel_per_rec*nrec*nbyte_cur);
 
- /* Seek to start of first record to read */
- nseek=(off_base+off_pan_start)*nbyte_cur;
- if ( koff != 0 ) {
-  nseek=nseek+
-   (long)koff*
-   (long)panel_size*
-   (long)panel_size*
-   (long)13*
-   (long)nbyte_cur;
+ /* Loop over kcount levels, starting from level 1 (the surface) */
+ for (koff=0;koff<kcount;++koff) {
+  /* Seek to start of first record to read */
+  nseek=(off_base+off_pan_start)*nbyte_cur;
+  if ( koff != 0 ) {
+   nseek=nseek+
+    (long)koff*
+    (long)panel_size*
+    (long)panel_size*
+    (long)13*
+    (long)nbyte_cur;
+  }
+  fseek(fin_hndl,(long)nseek,SEEK_SET);
+  i0=0;
+  bstride=(panel_size*3-nel_per_rec)*nbyte_cur;
+  for (j=0;j<nrec;++j){
+   fread(reg_buf+i0,nbyte_cur,nel_per_rec,fin_hndl);
+   fseek(fin_hndl,(long)bstride,SEEK_CUR);
+   i0=i0+nel_per_rec*nbyte_cur;
+  }
+  fwrite(reg_buf,nbyte_cur,nel_per_rec*nrec,fout_hndl);
  }
- fseek(fin_hndl,(long)nseek,SEEK_SET);
- i0=0;
- bstride=(panel_size*3-nel_per_rec)*nbyte_cur;
- for (j=0;j<nrec;++j){
-  fread(reg_buf+i0,nbyte_cur,nel_per_rec,fin_hndl);
-  fseek(fin_hndl,(long)bstride,SEEK_CUR);
-  i0=i0+nel_per_rec*nbyte_cur;
- }
- fwrite(reg_buf,nbyte_cur,nel_per_rec*nrec,fout_hndl);
 
  fclose(fout_hndl);
  fclose(fin_hndl);
